bd255716b628231dd1c63478b865b3fbc47aabd0
rehanrizvi84 <alok.srivastava@wipro.com>
2018-07-18 11:19:09 -0600

    osnir change

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -14,6 +14,7 @@ public class Application {
 
     @Bean
     public ServletRegistrationBean getActionServlet(ActionServlet actionServlet) {
+        // map /moviefun path but it wont work.
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
